05f8e688715ce7ffa60086d04424175eb32a4821
dakarl <dakarl@asu.edu>
2016-08-10 11:34:18 -0500

    Finished

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -1,47 +1,90 @@
 package day3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		File file = new File("C:/workspace/day2/day3/src/day3/test.txt");
+		File file = new File("student.txt");
 		
-		BufferedReader read = null;
-		try
+		if(!file.exists())
 		{
-			read = new BufferedReader(new FileReader(file));
-			System.out.println(read.readLine());
-
-		} 
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if ( read!= null)
+			BufferedWriter bw = null;
+			try
 			{
+				Student david = new Student("David","Phoenix","Arizona");
+				OutputStream out = new FileOutputStream(file);
+				bw = new BufferedWriter(new OutputStreamWriter(out));
+				bw.write(david.toString());
+			} 
+			catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+					try
+					{
+						bw.close();
+					} catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+			}
+		}
+			else
+			{
+				BufferedReader bw = null;
 				try
 				{
-					read.close();
-				}
-				catch (IOException e)
+					bw = new BufferedReader(new FileReader(file));
+					Student newStudent = new Student(bw.readLine(),bw.readLine(),bw.readLine());
+					System.out.println(newStudent);
+				} 
+				catch (FileNotFoundException e)
 				{
 					e.printStackTrace();
 				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}	
+				finally
+				{
+					try
+					{
+						bw.close();
+					} catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
 			}
-		}
-		
-		
 	}
 }
+			
+			
+			
+
+			
+			
+			
+			
+			
+
+		
