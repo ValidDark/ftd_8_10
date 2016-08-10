712132a4039ae495bfac0c8c3c31b61b70c50701
dakarl <dakarl@asu.edu>
2016-08-10 14:36:33 -0500

    nothing really

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -3,13 +3,10 @@ package day3;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class Main
@@ -17,74 +14,66 @@ public class Main
 	public static void main(String[] args)
 	{
 		File file = new File("student.txt");
-		
-		if(!file.exists())
+
+		if (!file.exists())
 		{
 			BufferedWriter bw = null;
 			try
 			{
-				Student david = new Student("David","Phoenix","Arizona");
-				OutputStream out = new FileOutputStream(file);
-				bw = new BufferedWriter(new OutputStreamWriter(out));
+				Student david = new Student("David", "Phoenix", "Arizona");
+				bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
 				bw.write(david.toString());
-			} 
+			}
 			catch (FileNotFoundException e)
 			{
 				e.printStackTrace();
 			}
-			catch(IOException e)
+			catch (IOException e)
 			{
 				e.printStackTrace();
 			}
 			finally
 			{
-					try
-					{
-						bw.close();
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-			}
-		}
-			else
-			{
-				BufferedReader bw = null;
 				try
 				{
-					bw = new BufferedReader(new FileReader(file));
-					Student newStudent = new Student(bw.readLine(),bw.readLine(),bw.readLine());
-					System.out.println(newStudent);
-				} 
-				catch (FileNotFoundException e)
-				{
-					e.printStackTrace();
-				}
-				catch(IOException e)
-				{
-					e.printStackTrace();
-				}	
-				finally
-				{
-					try
-					{
+					if (bw != null)
 						bw.close();
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
 				}
 			}
+		}
+		else
+		{
+			BufferedReader br = null;
+			try
+			{
+				br = new BufferedReader(new FileReader(file));
+				Student newStudent = new Student(br.readLine(), br.readLine(), br.readLine());
+				System.out.println(newStudent);
+			}
+			catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try
+				{
+					if (br != null)
+						br.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
-			
-			
-			
-
-			
-			
-			
-			
-			
-
-		
